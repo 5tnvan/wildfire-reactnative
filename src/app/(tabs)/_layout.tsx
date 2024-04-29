2a03e416839e20fdb09d91084e840a07a0d523a7
vtasst <5tnvan@gmail.com>
2024-04-29 08:11:04 +0200

    init expo project

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,9 +3,9 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import Colors from '@/src/app/constants/Colors';
+import { useColorScheme } from '@/src/app/components/useColorScheme';
+import { useClientOnlyValue } from '@/src/app/components/useClientOnlyValue';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
