47d0073b3a99141522fd7e93503c3be5719a0ed8
vtasst <5tnvan@gmail.com>
2024-05-28 20:31:07 +0200

    infinite scroll paused

--- src/app/(protected)/threesecs.tsx
+++ src/app/(protected)/threesecs.tsx
@@ -1,10 +1,11 @@
 import { View, Dimensions } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import InfiniteScroll from '../../components/InfiniteScroll';
+import InfiniteScrollRN from '@/src/components/InfiniteScrollRN';
 
 export default function ThreeSecondsScreen() {
 
   return (
-    <InfiniteScroll />
+    <InfiniteScrollRN />
   );
 }
