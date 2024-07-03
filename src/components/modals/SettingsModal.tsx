bccf2a00293e20ad100cca376a2f03506674432d
vtasst <5tnvan@gmail.com>
2024-07-03 13:19:31 +0200

    add delete button

--- src/components/modals/SettingsModal.tsx
+++ src/components/modals/SettingsModal.tsx
@@ -1,6 +1,6 @@
-import { Modal, Pressable, ScrollView, useColorScheme } from 'react-native';
+import { Alert, Linking, Modal, Pressable, ScrollView, useColorScheme } from 'react-native';
 import { Text, View } from "@/src/components/Themed";
-import { Ionicons } from '@expo/vector-icons';
+import { Entypo, Ionicons } from '@expo/vector-icons';
 import { PressableAnimated } from '../pressables/PressableAnimated';
 import { supabase } from '@/src/lib/supabase';
 
@@ -35,8 +35,20 @@ export function SettingsModal({ visible, data, onClose }: Props) {
                     <Text className='text-lg font-medium self-center'>Settings</Text>
                     <Text className='text-lg font-medium self-center'>{`     `} </Text>
                 </View>
-                <View className="justify-start h-full p-5">
-                    <PressableAnimated className='items-center' onPress={() => supabase.auth.signOut()}><Text>Log out</Text></PressableAnimated>
+                <View className="justify-start h-full p-5 gap-2">
+                    <PressableAnimated className='items-center' onPress={() => supabase.auth.signOut()}><Text className='font-semibold'>Log out </Text><Entypo name="log-out" size={14} color={colorScheme == 'dark' ? 'white' : 'grey'} /></PressableAnimated>
+                    <PressableAnimated className='items-center' onPress={() => 
+                    {                       
+                        Alert.alert("Visit https://www.3seconds.me/account/delete-account to delete account");
+                        (Linking.openURL('https://www.3seconds.me/account/delete-account'))
+                    }                        
+                    }><Text>Delete account</Text></PressableAnimated>
+                    <PressableAnimated className='items-center' onPress={() => 
+                    {                       
+                        Alert.alert("Visit https://www.3seconds.me/account/delete-data to delete data");
+                        (Linking.openURL('https://www.3seconds.me/account/delete-data'))
+                    }                        
+                    }><Text>Delete data, keep account</Text></PressableAnimated>
                 </View>
             </View>
         </Modal>
